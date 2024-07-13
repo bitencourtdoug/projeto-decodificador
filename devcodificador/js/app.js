03abc2e88930270e206f91e6f8d035c1fce3a34a
Douglas <douglasbitencourtadm@gmail.com>
2024-07-13 19:50:19 -0300

    Layout

--- devcodificador/js/app.js
+++ devcodificador/js/app.js
@@ -4,13 +4,13 @@ var textEncoded = document.querySelector("#encoded");
 function encrypt() {
     var encryptedText = textInformed.value.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     
-    document.getElementById('encoded').innerHTML = '<textarea cols="60" rows="12" id="original__text__decoded">' + encryptedText + '</textarea>' + '<button class="copy" id="copy" onclick="copy()">Copiar</button>'
+    document.getElementById('encoded').innerHTML = '<textarea cols="50" rows="16" id="original__text__decoded">' + encryptedText + '</textarea>' + '<button class="copy" id="copy" onclick="copy()">Copiar</button>'
 }
 
 function decrypt() {
     var decryptText = textInformed.value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     
-    document.getElementById('encoded').innerHTML = '<textarea cols="60" rows="12" id="original__text__decoded">' + decryptText + '</textarea>' + '<button class="copy" id="copy" onclick="copy()">Copiar</button>'
+    document.getElementById('encoded').innerHTML = '<textarea cols="50" rows="16" id="original__text__decoded">' + decryptText + '</textarea>' + '<button class="copy" id="copy" onclick="copy()">Copiar</button>'
 }
 
 function copy() {

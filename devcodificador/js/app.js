b8ec8614ca247bd4757dd6db3a3d5b2d51ae50cf
Douglas <douglasbitencourtadm@gmail.com>
2024-07-15 20:19:25 -0300

    Copy and layout button

--- devcodificador/js/app.js
+++ devcodificador/js/app.js
@@ -1,25 +1,33 @@
 var textInformed = document.querySelector("#original__text__decoded");
 var textEncoded = document.querySelector("#encoded");
-var button = '<button class="copy" id="copy" onclick="copy()">Copiar</button>' 
-var result ='<textarea cols="50" rows="16" id="encoded__encoded"></textarea>'
+var copyButton = '<button id="copy" onclick="copy()">Copiar</button>' 
+var result = '<img src="../assets/High quality products.png" alt="Desenho de um menino segurando uma lupa" >'      
 
 function encrypt() {
     var encryptedText = textInformed.value.replace(/e/g, "enter").replace(/i/g, "imes").replace(/a/g, "ai").replace(/o/g, "ober").replace(/u/g, "ufat");
     
-    document.getElementById('encoded').innerHTML =  encryptedText  + button
-    
-    }
+    document.getElementById('encoded').innerHTML = '<textarea id="inputText">' + encryptedText + '</textarea>'  + copyButton   
+    }    
 
 function decrypt() {
     var decryptText = textInformed.value.replace(/enter/g, "e").replace(/imes/g, "i").replace(/ai/g, "a").replace(/ober/g, "o").replace(/ufat/g, "u");
     
-    document.getElementById('encoded').innerHTML = decryptText + button
-}
+    document.getElementById('encoded').innerHTML ='<textarea id="inputText">' + decryptText + '</textarea>'  + copyButton     
+    }
 
 function copy() {
-    var textArea = document.getElementById("original__text__decoded");  
+    var textArea = document.getElementById("inputText");
     
     textArea.select();
     document.execCommand('copy');
     alert("Texto copiado com sucesso!");
+    limpar();
+}
+
+function limpar() {
+    original__text__decoded.value = "";
+    inputText.value = "";
+    document.getElementById('encoded').innerHTML = result;
+
+  
 }
